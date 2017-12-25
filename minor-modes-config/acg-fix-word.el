4ac389b62dce73135529cccd3126bf6fa9db26a5
Vitor Finotti Ferreira <vfinotti@gmail.com>
2017-12-25 20:37:08 -0200

    Commenting keybindings that contains "M-w"

--- minor-modes-config/acg-fix-word.el
+++ minor-modes-config/acg-fix-word.el
@@ -1,7 +1,7 @@
 (require 'fix-word)
-(global-set-key (kbd "M-w u") #'fix-word-upcase)
-(global-set-key (kbd "M-w l") #'fix-word-downcase)
-(global-set-key (kbd "M-w c") #'fix-word-capitalize)
+;(global-set-key (kbd "M-w u") #'fix-word-upcase)
+;(global-set-key (kbd "M-w l") #'fix-word-downcase)
+;(global-set-key (kbd "M-w c") #'fix-word-capitalize)
 
 (global-set-key (kbd "M-c") 'acg-toggle-letter-case)
 
