7a1ba75f4c481e4b87c846dafb8d005db720aeff
Vitor Finotti <vfinotti@gmail.com>
2018-01-11 19:41:27 -0200

    Preventing computer beep

--- acg-core.el
+++ acg-core.el
@@ -31,6 +31,13 @@
 ;; Lets start with a smattering of sanity
 (require 'sane-defaults)
 
+;; Don't beep. Don't visible-bell (fails on el capitan). Just blink the modeline on errors.
+
+(setq visible-bell nil)
+(setq ring-bell-function (lambda ()
+                           (invert-face 'mode-line)
+                           (run-with-timer 0.05 nil 'invert-face 'mode-line)))
+
 
 ;; Adding Repositories
 (require 'package)
